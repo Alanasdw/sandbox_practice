b9edab152e3e015954cfb7b56a8463db8720142c
Alanasdw <alanasdw@gmail.com>
2021-01-27 22:20:31 +0800

    still fiddling with the pthread things

--- sandbox.c
+++ sandbox.c
@@ -2,8 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// used in the open/openat functions
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+// need to be compiled with -pthread
+// used in the watcher routine
+#include <pthread.h>
+
+
 int main( void)
 {
-    
+    // think of a way to get the file io and the limits that are needed
+
+    // need to check the file io permissions
+
+    // multiprocess needed here
+
     return 0;
 }
