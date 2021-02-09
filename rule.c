d3cdff5e8d912ff1da5625440243ac463ea05b13
Alanasdw <alanasdw@gmail.com>
2021-02-09 22:48:04 +0800

    modified rules a bit

--- rule.c
+++ rule.c
@@ -2,12 +2,12 @@
 
 int32_t add_rule( char *program_name)
 {
-    if ( prctl( PR_SET_NO_NEW_PRIVS, 1) == -1)
-    {
-        perror( "prctl failed");
+    // if ( prctl( PR_SET_SECCOMP, SECCOMP_MODE_STRICT) == -1)
+    // {
+    //     perror( "prctl failed");
 
-        return 1;
-    }// if
+    //     return 1;
+    // }// if
     
     scmp_filter_ctx ctx;
 
@@ -16,30 +16,41 @@ int32_t add_rule( char *program_name)
     // can only call execve( "./main")
     if ( seccomp_rule_add( ctx, SCMP_ACT_KILL, SCMP_SYS( execve), 1, SCMP_A0( SCMP_CMP_NE, ( scmp_datum_t) program_name)) != 0)
     {
-        perror( "seccomp failed");
+        perror( "seccomp execve failed");
 
         return 1;
     }// if
     
     int32_t illegal_syscall[] = { SCMP_SYS( fork), SCMP_SYS( kill),
-                                SCMP_SYS( clone), SCMP_SYS( open),
-                                SCMP_SYS( openat), SCMP_SYS( vfork)};
+                                SCMP_SYS( clone)/*, SCMP_SYS( open),
+                                SCMP_SYS( openat)*/, SCMP_SYS( vfork)};
 
     // add all rules
-    for ( int32_t i = 0; i < 6 ; i++)
+    for ( int32_t i = 0; i < 4 ; i++)
     {
         if ( seccomp_rule_add( ctx, SCMP_ACT_KILL, illegal_syscall[ i], 0) != 0)
         {
-            perror( "seccomp failed");
+            perror( "seccomp general failed");
         
             return 1;
         }// if
         
     }// for i
-    
+
+    // open/openat need to have their own rules
+    if ( seccomp_rule_add( ctx, SCMP_ACT_KILL, SCMP_SYS( open), 1, SCMP_CMP( 1, SCMP_CMP_MASKED_EQ, O_WRONLY, O_WRONLY)) != 0 ||
+        seccomp_rule_add( ctx, SCMP_ACT_KILL, SCMP_SYS( open), 1, SCMP_CMP( 1, SCMP_CMP_MASKED_EQ, O_RDWR, O_RDWR)) != 0 ||
+        seccomp_rule_add( ctx, SCMP_ACT_KILL, SCMP_SYS( openat), 1, SCMP_CMP( 1, SCMP_CMP_MASKED_EQ, O_WRONLY, O_WRONLY)) != 0 ||
+        seccomp_rule_add( ctx, SCMP_ACT_KILL, SCMP_SYS( openat), 1, SCMP_CMP( 1, SCMP_CMP_MASKED_EQ, O_RDWR, O_RDWR)) != 0)
+    {
+        perror( "seccomp open/openat failed");
+
+        return 1;
+    }// if
+
     if ( seccomp_load( ctx) != 0)
     {
-        perror( "seccomp failed");
+        perror( "seccomp load failed");
         
         return 1;
     }// if
--- rule.h
+++ rule.h
@@ -3,8 +3,10 @@
 #include <seccomp.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <fcntl.h>
 
-#include <sys/prctl.h>
+// #include <sys/prctl.h>
+// #include <linux/seccomp.h>
 
 
 int32_t add_rule( char *program_name);
